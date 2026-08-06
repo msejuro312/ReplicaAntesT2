38e66f411bcbb25e3ab7df2fd65533699be82648
msejuro312 <msejuro312@protonmail.com>
2026-08-06 17:59:10 -0500

    Application (archivo de arranque)

---
+++ src/main/java/cibertec/demo/ReplicaAntesT2Application.java
@@ -0,0 +1,12 @@
+package cibertec.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicaAntesT2Application {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ReplicaAntesT2Application.class, args);
+    }
+}
